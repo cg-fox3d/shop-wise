dee570d69618f3a1776b6a367fe859c68af7b204
cg-fox3d <manojmodugala@gmail.com>
2025-05-31 15:52:08 +0000

    updated inline-scripts style-src

--- next.config.js
+++ next.config.js
@@ -18,7 +18,7 @@ const nextConfig = {
     const checkoutCSP = `
       default-src 'self';
       script-src 'self' 'unsafe-inline' https://www.gstatic.com https://www.google.com https://checkout.razorpay.com;
-      style-src 'self' https://cdnjs.cloudflare.com https://fonts.googleapis.com;
+      style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com https://fonts.googleapis.com;
       img-src 'self' data: https://picsum.photos https://www.google.com;
       font-src 'self' https://fonts.gstatic.com;
       connect-src 'self' https://firestore.googleapis.com https://*.firebaseio.com https://firebase.googleapis.com https://www.googleapis.com https://www.gstatic.com https://securetoken.googleapis.com https://identitytoolkit.googleapis.com https://api.razorpay.com https://lumberjack.razorpay.com https://numbers-guru.netlify.app;
